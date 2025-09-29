b888621da4c55c1823afd7adad6f784beb046183
2024CA101 <shwetasingh5082770s@gmail.com>
2025-09-29 13:31:55 +0530

    Continue frontend setup

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <h1 className="text-4xl text-blue-500 font-bold">Welcome to EchoesOfArt!</h1>
+    </div>
+  );
+}
