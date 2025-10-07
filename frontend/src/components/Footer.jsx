4513707f03c3576e372b0f1a40892247e6aea4ce
paras-codess <paras.gupta16920@gmail.com>
2025-10-07 23:43:54 +0530

    some change in landing page and header, footer

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./../styles/Footer.css";
+import { FaTwitter, FaFacebookF, FaInstagram, FaTelegramPlane } from "react-icons/fa";
 
 const Footer = () => {
   return (
@@ -13,21 +14,30 @@ const Footer = () => {
 
         {/* Center: Quick Links */}
         <div className="footer-center">
-          <a href="#upload">Upload</a>
-          <a href="#profile">Profile</a>
-          <a href="#saved">Saved Art</a>
+          <a href="#home">Home</a>
           <a href="#about">About</a>
           <a href="#contact">Contact</a>
+          <a href="#Feedback">Feedback</a>
         </div>
 
-        {/* Right: Social Links */}
-        <div className="footer-right">
-          <a href="https://twitter.com" target="_blank" rel="noreferrer">Twitter</a>
-          <a href="https://facebook.com" target="_blank" rel="noreferrer">Facebook</a>
-          <a href="https://instagram.com" target="_blank" rel="noreferrer">Instagram</a>
+        {/* Right: Social Links with Rounded Bubbles */}
+        <div className="footer-right footer-social">
+          <a href="https://twitter.com" target="_blank" rel="noreferrer">
+            <FaTwitter />
+          </a>
+          <a href="https://facebook.com" target="_blank" rel="noreferrer">
+            <FaFacebookF />
+          </a>
+          <a href="https://instagram.com" target="_blank" rel="noreferrer">
+            <FaInstagram />
+          </a>
+          <a href="https://t.me/username" target="_blank" rel="noreferrer">
+            <FaTelegramPlane />
+          </a>
         </div>
       </div>
 
+      {/* Bottom copyright */}
       <div className="footer-bottom">
         &copy; {new Date().getFullYear()} Echoes of Art. All rights reserved.
       </div>
