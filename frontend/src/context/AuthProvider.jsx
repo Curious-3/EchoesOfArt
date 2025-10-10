168a29f432725b9036039199742f8aca71e4691a
paras-codess <paras.gupta16920@gmail.com>
2025-10-10 18:27:59 +0530

    Some changes and sign up fix

--- frontend/src/context/AuthProvider.jsx
+++ frontend/src/context/AuthProvider.jsx
@@ -4,7 +4,7 @@ import axios from "axios";
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [user, setUser] = useState(null);
+  const [user, setUser] = useState(localStorage.getItem("user") ? JSON.parse(localStorage.getItem("user")) : null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
